097883725a4370a222dbb6e5cf5072e7b0eba552
Harsh Gangwal <harshgangwal24@gmail.com>
2024-10-15 22:08:11 +0530

    finish

---
+++ portfolio_website/src/common/AboutSection.jsx
@@ -0,0 +1,11 @@
+
+
+// function AboutSection({h4,p}) {
+//   return (<>
+//     <h4>{h4}</h4>
+//     <p >{p}</p>
+//     </>
+//   );
+// }
+
+// export default AboutSection
--- portfolio_website/src/sections/About/About.jsx
+++ portfolio_website/src/sections/About/About.jsx
@@ -1,19 +1,44 @@
-import styles from './AboutStyles.module.css';
-import heroimg from "../../assets/hero.jpg";
+// import styles from './AboutStyles.module.css';
+// import Aboutsection from '../../common/AboutSection';
 
-function About() {
-  return (
-    <section id='about' className={styles.container}>
-<div className={styles.colorModeContainer}>
-        <img
-          className={styles.hero}
-          src={heroimg}
-          alt="Profile picture of Harsh Gangwal"
-        />
-      </div>
 
-    </section>
-  )
-}
+// function About() {
+//   return (
+//     <section id='about' className={styles.container}>
+// <div className={styles.Title}>
+//   <h1>About Me</h1> 
+//       </div >
+//       <div className={styles.content}>
+//         </div>
 
-export default About
+//         <div className={styles.para}>
+//       <Aboutsection
+//       h4= "👋 Who Am I?"
+//       p=" pre-final year Computer Science student with a passion for web development, focused on building user-friendly and visually engaging websites."
+//       />
+//       <Aboutsection
+//         h4="💻 Front-End Development"
+//         p="I specialize in front-end technologies such as HTML, CSS, JavaScript, and React, creating responsive and intuitive user interfaces."
+//         />
+//        < Aboutsection 
+//        h4="🔧 Back-End Knowledge"
+//        p="I possess intermediate experience with backend technologies like Node.js and SQL, enabling me to contribute to full-stack development."
+       
+//        />
+//         < Aboutsection 
+//        h4="🛠 Version Control & Collaboration"
+//        p="I am proficient in Git for version control, thrive in collaborative environments, and enjoy working as part of a team to solve problems effectively."
+       
+//        />
+//         < Aboutsection 
+//        h4="📚 Continuous Learning"
+//        p="I am dedicated to continuous learning and consistently work on improving my skills by staying up-to-date with the latest technologies and trends in web development."
+       
+//        />
+//        </div>
+     
+//     </section>
+//   )
+// }
+
+// export default About
